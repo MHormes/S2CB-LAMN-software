e0fd29ad6acc8f26f6c729d91cea2533fd482dca
Alessandro Busacchi <I459847@fhict.nl>
2021-03-24 19:39:19 +0100

    two other excteptions included

---
+++ LAMN-Software/LAMN-Software/Exceptions/IncorrectPriceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMN_Software.Exceptions
+{
+    class IncorrectPriceException : Exception
+    {
+        public IncorrectPriceException(string price) : base($"{price} is not in correct format (1/ 1.40)") { }
+    }
+}
