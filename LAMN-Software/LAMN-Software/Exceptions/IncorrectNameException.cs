6da230369cafe62ff8ae18ca622cce95a4113071
Alessandro Busacchi <I459847@fhict.nl>
2021-03-24 12:23:53 +0100

    new exceptions and dedicated folder

---
+++ LAMN-Software/LAMN-Software/Exceptions/IncorrectNameException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAMN_Software
+{
+    class IncorrectNameException : Exception
+    {
+        public IncorrectNameException(string name) : base($"{name} is not in correct format (Numbers not accepted)") { }
+    }
+}
