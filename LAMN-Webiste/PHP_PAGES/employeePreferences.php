ecac311a11ab4bb8296d6a4417cf2b771786adee
Alessandro Busacchi <I459847@fhict.nl>
2021-06-17 13:57:57 +0200

    no preferences not added

--- LAMN-Webiste/PHP_PAGES/employeePreferences.php
+++ LAMN-Webiste/PHP_PAGES/employeePreferences.php
@@ -5,13 +5,16 @@ include "../DatabaseConn/preferences_template.php";
 $user = $_SESSION['Username'];
 $empBSN = getBSN($user);
 
+$shifts = getShifts($empBSN);
+var_dump($shifts);
+
 if (isset($_POST['btnSave'])) {
     $isPresent = preferencesPresent($empBSN);
 
-    if($isPresent == true){
-    removePreferences($empBSN);    
+    if ($isPresent == true) {
+        removePreferences($empBSN);
     }
-    
+
     addPreference('MONDAY', $empBSN, $_POST['mondayShifts']);
     addPreference('TUESDAY',  $empBSN, $_POST['tuesdayShifts']);
     addPreference('WEDNESDAY',  $empBSN, $_POST['wednesdayShifts']);
@@ -24,8 +27,8 @@ if (isset($_POST['btnSave'])) {
 if (isset($_POST['btnDelete'])) {
     $isPresent = preferencesPresent($empBSN);
 
-    if($isPresent == true){
-    removePreferences($empBSN);    
+    if ($isPresent == true) {
+        removePreferences($empBSN);
     }
 }
 ?>
@@ -49,83 +52,84 @@ if (isset($_POST['btnDelete'])) {
     <?php include '../navigationBar.php'; ?>
 
     <section class="employeePreferences-form">
-            <div class="row">
-                <h2>Shift preferences</h2>
-            </div>
-            <div class="row">
-                <form method="post" action="#" class="contact-form">
-                    <div class="row">
-                        <label for="monday">Monday</label>
-                            <select name="mondayShifts" id="mondayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label for="tuesday">Tuesday</label>
-                            <select name="tuesdayShifts" id="tuesdayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label for="wednesday">Wednesday</label>
-                            <select name="wednesdayShifts" id="wednesdayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label for="thursday">Thursday</label>
-                            <select name="thursdayShifts" id="thursdayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label for="friday">Friday</label>
-                            <select name="fridayShifts" id="fridayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label for="saturday">Saturday</label>
-                            <select name="saturdayShifts" id="saturdayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <label for="sunday">Sunday</label>
-                            <select name="sundayShifts" id="sundayShifts">
-                            <option value=""></option>
-                            <option value="MORNING">MORNING</option>
-                            <option value="AFTERNOON">AFTERNOON</option>
-                            <option value="EVENING">EVENING</option>
-                        </select>
-                    </div>
-                    <div class="row">
-                        <input type="submit" value="Save" name="btnSave">
-                    </div>
-                    <div class="row">
-                        <input type="submit" value="Delete" name="btnDelete">
-                    </div>
-                </form>
-            </div>
-        </section>
+        <div class="row">
+            <h2>Shift preferences</h2>
+        </div>
+
+        <div class="row">
+            <form method="post" action="#" class="contact-form">
+                <div class="row">
+                    <label for="monday">Monday</label>
+                    <select name="mondayShifts" id="mondayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <label for="tuesday">Tuesday</label>
+                    <select name="tuesdayShifts" id="tuesdayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <label for="wednesday">Wednesday</label>
+                    <select name="wednesdayShifts" id="wednesdayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <label for="thursday">Thursday</label>
+                    <select name="thursdayShifts" id="thursdayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <label for="friday">Friday</label>
+                    <select name="fridayShifts" id="fridayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <label for="saturday">Saturday</label>
+                    <select name="saturdayShifts" id="saturdayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <label for="sunday">Sunday</label>
+                    <select name="sundayShifts" id="sundayShifts">
+                        <option value=""></option>
+                        <option value="MORNING">MORNING</option>
+                        <option value="AFTERNOON">AFTERNOON</option>
+                        <option value="EVENING">EVENING</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <input type="submit" value="Save" name="btnSave">
+                </div>
+                <div class="row">
+                    <input type="submit" value="Delete" name="btnDelete">
+                </div>
+            </form>
+        </div>
+    </section>
 
 
 </body>
