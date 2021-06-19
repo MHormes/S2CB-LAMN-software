3685a1b4743ebd5c9506e3348f765d5f05f07e4d
Luc van der Putten <luc.putten70@gmail.com>
2021-06-19 23:19:16 +0200

    Holidays

--- LAMN-Webiste/navigationBar.php
+++ LAMN-Webiste/navigationBar.php
@@ -5,4 +5,5 @@
   <li><a href="../PHP_PAGES/profile.php">Profile</a></li>
   <li><a href="../PHP_PAGES/index.php">Logout</a></li>
   <li><a href="../PHP_PAGES/employeePreferences.php">Prefences</a></li>
+  <li><a href="../PHP_PAGES/employeeHolidays.php">Holidays</a></li>
 </ul>
