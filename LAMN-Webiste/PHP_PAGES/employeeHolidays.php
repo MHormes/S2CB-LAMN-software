bc7a6cad9cbece9d77542be3c2ac1fd263e32fc2
Luc van der Putten <luc.putten70@gmail.com>
2021-06-20 22:16:24 +0200

    Holidays week + Sick Days

--- LAMN-Webiste/PHP_PAGES/employeeHolidays.php
+++ LAMN-Webiste/PHP_PAGES/employeeHolidays.php
@@ -10,13 +10,13 @@ if (isset($_POST['btnSave'])) {
 
     
 
-    addHolidays('MONDAY', $empBSN, $_POST['mondayHolidays']);
-    addHolidays('TUESDAY',  $empBSN, $_POST['tuesdayHolidays']);
-    addHolidays('WEDNESDAY',  $empBSN, $_POST['wednesdayHolidays']);
-    addHolidays('THURSDAY',  $empBSN, $_POST['thursdayHolidays']);
-    addHolidays('FRIDAY',  $empBSN, $_POST['fridayHolidays']);
-    addHolidays('SATURDAY',  $empBSN, $_POST['saturdayHolidays']);
-    addHolidays('SUNDAY',  $empBSN, $_POST['sundayHolidays']);
+    addHolidays($_POST['weekSelecter'],'MONDAY', $empBSN, $_POST['mondayHolidays']);
+    addHolidays($_POST['weekSelecter'],'TUESDAY',  $empBSN, $_POST['tuesdayHolidays']);
+    addHolidays($_POST['weekSelecter'],'WEDNESDAY',  $empBSN, $_POST['wednesdayHolidays']);
+    addHolidays($_POST['weekSelecter'],'THURSDAY',  $empBSN, $_POST['thursdayHolidays']);
+    addHolidays($_POST['weekSelecter'],'FRIDAY',  $empBSN, $_POST['fridayHolidays']);
+    addHolidays($_POST['weekSelecter'],'SATURDAY',  $empBSN, $_POST['saturdayHolidays']);
+    addHolidays($_POST['weekSelecter'],'SUNDAY',  $empBSN, $_POST['sundayHolidays']);
 }
 
 if (isset($_POST['btnDelete'])) {
@@ -52,6 +52,12 @@ if (isset($_POST['btnDelete'])) {
 
         <div class="row">
             <form method="post" action="#" class="contact-form">
+            </br>
+        <p>Choose the week you want to plan your Holiday and click 'Show'</p>
+        </br>
+        <input class="weekCounter" type="number" name="weekSelecter" min="0" max="52" step="1" value="<?php echo $weekNmr ?>">
+        <input class="button" type="submit" name="btnSelectWeek" value="Show">
+        </br>
                 <div class="row">
                     <label for="monday">Monday</label>
                     <select name="mondayHolidays" id="mondayHolidays">
--- LAMN-Webiste/navigationBar.php
+++ LAMN-Webiste/navigationBar.php
@@ -6,4 +6,5 @@
   <li><a href="../PHP_PAGES/index.php">Logout</a></li>
   <li><a href="../PHP_PAGES/employeePreferences.php">Prefences</a></li>
   <li><a href="../PHP_PAGES/employeeHolidays.php">Holidays</a></li>
+  <li><a href="../PHP_PAGES/employeeSickDays.php">Sick Days</a></li>
 </ul>
